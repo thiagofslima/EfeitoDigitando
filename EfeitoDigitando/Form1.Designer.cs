7253b03ebc3621c6d9b625259a65e6c9008571c8
Thiago Lima <thiagofslima@live.com>
2022-04-11 15:51:45 -0300

    Adicionar arquivos de projeto.

---
+++ EfeitoDigitando/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EfeitoDigitando
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblDigitando = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(270, 115);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblDigitando
+            // 
+            this.lblDigitando.AutoSize = true;
+            this.lblDigitando.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigitando.Location = new System.Drawing.Point(17, 9);
+            this.lblDigitando.MaximumSize = new System.Drawing.Size(330, 0);
+            this.lblDigitando.Name = "lblDigitando";
+            this.lblDigitando.Size = new System.Drawing.Size(184, 26);
+            this.lblDigitando.TabIndex = 1;
+            this.lblDigitando.Text = "Efeito Digitando...";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(12, 117);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(252, 20);
+            this.txtTexto.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnIniciar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 154);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.lblDigitando);
+            this.Controls.Add(this.btnIniciar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Efeito Digitando...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblDigitando;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EfeitoDigitando/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EfeitoDigitando
+{
+    public partial class Form1 : Form
+    {
+        int letra;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            lblDigitando.Text = string.Empty;
+            letra = 0;
+            timer1.Enabled = true;
+            txtTexto.Enabled = false;
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (letra == txtTexto.TextLength - 1)
+                {
+                    timer1.Enabled = false;
+                    txtTexto.Enabled = true;
+                }
+
+                lblDigitando.Text += txtTexto.Text.Substring(letra, 1);
+                letra++;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
